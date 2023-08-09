18b52a0312a802cd276650f87eb8739888b7e211
jhenrique09 <jhsv09@gmail.com>
2023-08-09 22:29:23 +0000

    vendor: Drop SimpleDeviceConfig
    Change-Id: Iba7e9e042b037f1147647608de9ab03a21fa00f9

--- config/common.mk
+++ config/common.mk
@@ -116,10 +116,6 @@ PRODUCT_COPY_FILES += \
     vendor/hertzify/prebuilt/common/etc/init/init.hertzify-updater.rc:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/init/init.hertzify-updater.rc
 endif
 
-# Config
-PRODUCT_PACKAGES += \
-    SimpleDeviceConfig 
-
 # Disable RescueParty due to high risk of data loss
 PRODUCT_PRODUCT_PROPERTIES += \
     persist.sys.disable_rescue=true
