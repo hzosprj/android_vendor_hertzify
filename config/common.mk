7c1de460a6d0706dd2f58826452a6014add1ee3a
nift4 <nift4@protonmail.com>
2024-08-14 16:24:35 +0800

    config: Remove useless addon.d file
    * Avoid detection as custom ROM
    
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- config/common.mk
+++ config/common.mk
@@ -49,11 +49,7 @@ endif
 # Backup Tool
 PRODUCT_COPY_FILES += \
     vendor/hertzify/prebuilt/common/bin/backuptool.sh:install/bin/backuptool.sh \
-    vendor/hertzify/prebuilt/common/bin/backuptool.functions:install/bin/backuptool.functions \
-    vendor/hertzify/prebuilt/common/bin/50-hertzify.sh:$(TARGET_COPY_OUT_SYSTEM)/addon.d/50-hertzify.sh
-
-PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
-    system/addon.d/50-hertzify.sh
+    vendor/hertzify/prebuilt/common/bin/backuptool.functions:install/bin/backuptool.functions
 
 ifneq ($(strip $(AB_OTA_PARTITIONS) $(AB_OTA_POSTINSTALL_CONFIG)),)
 PRODUCT_COPY_FILES += \
