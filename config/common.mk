d0fc139f32d7d80b7871c5c6f0512c5d8b408ea9
someone5678 <59456192+someone5678@users.noreply.github.com>
2025-01-24 22:53:14 +0100

    config: Conditionally build ColumbusService
    Change-Id: I535e1b5f813922acbb9c0b39e0cc63ccb1f19ebc

--- config/common.mk
+++ config/common.mk
@@ -78,6 +78,12 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     vendor/hertzify/prebuilt/common/etc/init/init.hertzify-system_ext.rc:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/init/init.hertzify-system_ext.rc
 
+# ColumbusService
+ifneq ($(TARGET_SUPPORTS_QUICK_TAP),false)
+PRODUCT_PACKAGES += \
+    ColumbusService
+endif
+
 # Enable SIP+VoIP on all targets
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.software.sip.voip.xml
