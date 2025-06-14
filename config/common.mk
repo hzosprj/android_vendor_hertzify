bfee183c9d60a5e730c389fe993a9262e1ec2ed3
techyminati <sinha.aryan03@gmail.com>
2025-06-14 23:30:42 +0800

    config: Enable Material Design 3 Expressive
    Change-Id: Ib6d3ac2e0f3d76768c508e1b00efc51e141c1b09
    Signed-off-by: techyminati <sinha.aryan03@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -46,6 +46,10 @@ endif
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += persist.sys.strictmode.disable=true
 endif
 
+# Enable Material Design 3 Expressive
+PRODUCT_PRODUCT_PROPERTIES += \
+    is_expressive_design_enabled=true
+
 # Backup Tool
 PRODUCT_COPY_FILES += \
     vendor/hertzify/prebuilt/common/bin/backuptool.sh:install/bin/backuptool.sh \
