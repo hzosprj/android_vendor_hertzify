4519fc66a7f8c9a347aeef2ee496fea2e43ef3f7
Danny Lin <danny@kdrag0n.dev>
2025-07-01 12:44:48 +0000

    config: Disable touch video heatmap
    Change-Id: Iac858eeb059a854dbb020d14a0f165b41055511d

--- config/common.mk
+++ config/common.mk
@@ -144,7 +144,12 @@ PRODUCT_SYSTEM_EXT_PROPERTIES += \
 # Disable RescueParty due to high risk of data loss
 PRODUCT_PRODUCT_PROPERTIES += \
     persist.sys.disable_rescue=true
-    
+
+# Disable touch video heatmap to reduce latency, motion jitter, and CPU usage
+# on supported devices with Deep Press input classifier HALs and models
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.input.video_enabled=false
+ 
 # Extra tools
 PRODUCT_PACKAGES += \
     bash \
